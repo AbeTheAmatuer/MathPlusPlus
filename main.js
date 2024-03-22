4171cdbdfb711b7bc825178519a36cc0265a656e
Sir Abe <ibrahimkhurram2008@gmail.com>
2024-03-22 12:59:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-function sigma(r, n, opp, incVal){
-    for(let i = r; i < n; i++){
-        
+function sigma(uL, lL, opp, incVal){
+    let total = 0;
+    console.log("incval is " + incVal)
+    placeHolder = (incVal === "lL") ? true : false;
+    for(let i = lL; i <= uL; i++){
+        if(placeHolder){
+            incVal = i;
+        }
+        switch(opp){
+            case "+":
+                total += incVal
+                break;
+            case "-":
+                total -= incVal
+                break;
+            case "*":
+                total *= incVal
+                break;
+            case "/":
+                total /= incVal
+                break;
+            case "^":
+                total += (incVal * incVal)
+        }
     }
+    return(total);
 }
+
+
